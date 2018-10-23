c56ecb7d1d522c141ae25bf57c6c80445fb0bcc5
Wade Nichols <kwnichols73@gmail.com>
2018-10-23 14:39:34 -0600

    submitting backend of votes

--- exercises/rock-the-vote/server.js
+++ exercises/rock-the-vote/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 // ROUTES
-app.use("/posts", require("./routes/posts"));
+app.use("/api/posts", require("./routes/posts"));
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, () => console.log(`Connected to MongoDB`))
 app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`))
