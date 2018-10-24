a4b607189d285075a55eb5e9b52d01e2c2c2500c
Wade Nichols <kwnichols73@gmail.com>
2018-10-24 14:59:10 -0600

    start of project2 submition

---
+++ projects/pick-it-2.0/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+//third party
+
+
+
+//my components
+// import Search from "./Search";
+// import HowTo from "./"
+
+function App() {
+  return (
+    <div>
+      {/* <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/search" component={Search} />
+        <Route exact path="/howTo" component={How - to} />
+      </Switch> */}
+      <footer>©2018</footer>
+    </div>
+  );
+}
+
+export default App;
